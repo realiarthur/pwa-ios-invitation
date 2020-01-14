02e5e5dfb076ad1d4ce27b3782d3c7522027c2fa
aislamov <aislamov@aislamov-mbp15.local>
2020-01-14 14:52:16 +0200

    fix ipad with macintel platform

--- src/pwa-const.js
+++ src/pwa-const.js
@@ -1,12 +1,17 @@
-export const isIos = /iPad|iPhone|iPod/.test(window.navigator.userAgent);
+export const isIos =
+  (/iPad|iPhone|iPod/.test(window.navigator.userAgent) ||
+    (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)) &&
+  !window.MSStream;
 
-export const iosDevice = isIos
-  ? /iPad|iPhone|iPod/.exec(window.navigator.userAgent)[0]
+export let iosDevice = isIos
+  ? /iPad|iPhone|iPod/.exec(window.navigator.userAgent)
   : null;
+if (iosDevice) iosDevice = iosDevice[0];
+else if (isIos && navigator.platform === "MacIntel") iosDevice = "iPad";
 
 export const isPwa = !!(
   window.navigator.standalone ||
-  (window.matchMedia&&window.matchMedia("(display-mode: standalone)").matches)
+  (window.matchMedia && window.matchMedia("(display-mode: standalone)").matches)
 );
 
 // Detect if Safari
